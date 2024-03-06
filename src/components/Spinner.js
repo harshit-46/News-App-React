00be8cd31662e21e73cdaa8f6c6f3f883d2cf501
Harshit Gupta <iamharshit999@gmail.com>
2024-03-07 02:08:28 +0530

    Updated

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import spinner from './Spinner.gif'
+import React from 'react'
+import loading from './Spinner.gif'
 
-export default class Spinner extends Component {
-    render() {
+const Spinner = () => {
         return (
             <div className='text-center'>
-                <img className="my-3" src={spinner} alt="loading"/>
+                <img className="my-3" src={loading} alt="loading"/>
             </div>
         )
-    }
 }
+
+export default Spinner
